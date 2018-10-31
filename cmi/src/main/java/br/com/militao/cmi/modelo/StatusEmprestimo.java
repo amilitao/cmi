5dea440f99ddaf8cf1dcc9aae2190bda2a315dc2
adriano <adriano.militao@yahoo.com.br>
2018-10-31 18:29:42 -0300

    mudando a cor do icone da lista de emprestimo

--- cmi/src/main/java/br/com/militao/cmi/modelo/StatusEmprestimo.java
+++ cmi/src/main/java/br/com/militao/cmi/modelo/StatusEmprestimo.java
@@ -2,7 +2,7 @@ package br.com.militao.cmi.modelo;
 
 public enum StatusEmprestimo {
 	
-	INICIADO("1%", "iniciado", "brown"), EMISSAO_NFE("25%", "aguardando nfe", "deep-purple"), 
+	INICIADO("1%", "iniciado", "brown"), EMISSAO_NFE("25%", "aguardando nfe", "green"), 
 	AGUARDANDO_TRANSPORTE("40%", "aguardando transporte", "deep-purple"), ENVIADO("60%", "em transporte", "amber"),
 	EM_EMPRESTIMO("75%", "em emprestimo", "green"), ENCERRADO("100%", "encerrado", "red");
 	
