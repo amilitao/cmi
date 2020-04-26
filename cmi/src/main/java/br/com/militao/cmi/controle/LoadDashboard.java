c0581aff409a904d7f526b2f47c9408553a37c19
Adriano Militao <adriano.militao@yahoo.com.br>
2020-04-26 16:20:32 -0300

    refatoração

--- cmi/src/main/java/br/com/militao/cmi/controle/LoadDashboard.java
+++ cmi/src/main/java/br/com/militao/cmi/controle/LoadDashboard.java
@@ -1,50 +1,32 @@
 package br.com.militao.cmi.controle;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.militao.cmi.modelo.Dashboard;
 import br.com.militao.cmi.modelo.DashboardBuilder;
 import br.com.militao.cmi.modelo.componente.Componente;
 import br.com.militao.cmi.modelo.componente.ComponenteCreator;
 
-
 public class LoadDashboard implements Logica {
 
 	@Override
-	public String executa(HttpServletRequest req, HttpServletResponse resp){
+	public String executa(HttpServletRequest req, HttpServletResponse resp) {
 
-		Dashboard dashboard;
-		
-		HttpSession session = req.getSession();
+		ComponenteCreator creator = new ComponenteCreator();
+		Componente painelStatusImpressora = creator.create(ComponenteCreator.PAINEL_STATUS);
+		Componente painelUltimasAtualizacoes = creator.create(ComponenteCreator.PAINEL_ULT_ATUALIZACOES);
 
-		if (session.getAttribute("dashboard") == null) {
+		Dashboard dashboard = new DashboardBuilder().comPainelStatus(painelStatusImpressora.criar())
+				.comPainelUltimasAtualizacoes(painelUltimasAtualizacoes.criar()).geraDashboard();
 
-			ComponenteCreator creator = new ComponenteCreator();
-			Componente painelStatusImpressora = creator.create(ComponenteCreator.PAINEL_STATUS);		
-			Componente painelUltimasAtualizacoes = creator.create(ComponenteCreator.PAINEL_ULT_ATUALIZACOES);
+		// criar tabela no banco de dados
+		/*
+		 * NotificacaoDao notDao = new NotificacaoDao();
+		 * session.setAttribute("notificacoes", notDao.getList());
+		 */
 
-			dashboard = new DashboardBuilder().
-					comPainelStatus(painelStatusImpressora.criar()).					
-					comPainelUltimasAtualizacoes(painelUltimasAtualizacoes.criar()).
-					geraDashboard();
-
-			session.setAttribute("dashboard", dashboard);
-
-		} else {
-
-			dashboard = (Dashboard) session.getAttribute("dashboard");
-		}
-		
-		
-		//criar tabela no banco de dados
-		/*NotificacaoDao notDao = new NotificacaoDao();
-		session.setAttribute("notificacoes", notDao.getList());*/		
-			
-		
-		req.setAttribute("painelStatusImpressora", dashboard.getPainelStatusImpressora());		
+		req.setAttribute("painelStatusImpressora", dashboard.getPainelStatusImpressora());
 		req.setAttribute("painelUltimasAtualizacoes", dashboard.getPainelUltimasAtualizacoes());
 
 		return "forward:dashboard.jsp";
